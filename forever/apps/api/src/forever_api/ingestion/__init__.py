7fc492bffa353559e213ea6ac0986716def491de
IVA079 <anastasiasaat81@gmail.com>
2026-07-04 16:43:22 +0600

    delete all from forever

--- forever/apps/api/src/forever_api/ingestion/__init__.py
+++
@@ -1,4 +0,0 @@
-from forever_api.ingestion.source_pack import SourceChunk, SourcePack, SourcePackError, build_source_pack
-
-__all__ = ["SourceChunk", "SourcePack", "SourcePackError", "build_source_pack"]
-
--- forever/apps/api/src/forever_api/routers/__init__.py
+++
@@ -1,4 +0,0 @@
-from forever_api.routers.source_packs import create_source_pack_endpoint
-
-__all__ = ["create_source_pack_endpoint"]
-
